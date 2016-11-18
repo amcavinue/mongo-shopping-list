b91e50149bbb3e1eb90f5b85ae4c33a4907e4282
Aaron <aaronmcavinue@gmail.com>
2016-11-18 20:53:52 +0000

    Heroku config.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://amcavinue:Bassword53@ds157667.mlab.com:57667/mongo-shopping-list' :
-                            'mongodb://amcavinue:Bassword53@ds157667.mlab.com:57667/mongo-shopping-list');
+                            'mongodb://admin:abc123@ds157667.mlab.com:57667/mongo-shopping-list' :
+                            'mongodb://admin:abc123@ds157667.mlab.com:57667/mongo-shopping-list');
 exports.PORT = process.env.PORT || 8080;
